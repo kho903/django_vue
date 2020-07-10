50fd3cd1f4585f40080bd1522abd60917518c8aa
kho903 <gmldnr2222@naver.com>
2020-07-10 22:01:22 +0900

    Django mixin

--- todo/urls.py
+++ todo/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('vonly/', views.TodoVueOnlyTV.as_view(), name='vonly'),
     path('create/', views.TodoCV.as_view(), name='create'),
     path('list/', views.TodoLV.as_view(), name='list'),
-    path('<int:pk>/delete', views.TodoDelV.as_view(), name='delete')
+    path('<int:pk>/delete/', views.TodoDelV.as_view(), name='delete'),
+    path('mixin/', views.TodoMOMCV.as_view(), name='mixin'),
+    path('<int:pk>/delete2/', views.TodoDelV2.as_view(), name='delete2'),
+
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, CreateView, ListView, DeleteView
+from django.views.generic.list import MultipleObjectMixin
 
 from todo.models import Todo
 
@@ -24,3 +25,24 @@ class TodoDelV(DeleteView):
     model = Todo
     template_name = 'todo/todo_confirm_delete.html'
     success_url = reverse_lazy('todo:list')
+
+
+class TodoMOMCV(MultipleObjectMixin, CreateView):
+    model = Todo
+    fields = '__all__'
+    template_name = "todo/todo_form_list.html"
+    success_url = reverse_lazy('todo:mixin')
+
+    def get(self, request, *args, **kwargs):
+        self.object_list = self.get_queryset()
+        return super().get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        self.object_list = self.get_queryset()
+        return super().post(request, *args, **kwargs)
+
+
+class TodoDelV2(DeleteView):
+    model = Todo
+    template_name = 'todo/todo_confirm_delete.html'
+    success_url = reverse_lazy('todo:mixin')
